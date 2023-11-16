5cb6c79dafe69bbc4dbfbe6fc5c9ec20a4dce714
xytos99 <134260005+xytos99@users.noreply.github.com>
2023-11-16 11:57:43 +0530

    Add scorekeeping

--- script.js
+++ script.js
@@ -9,14 +9,22 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
 
   if ( playerSelection === computerSelection) {
+    tie += 1;
     return "It's a tie!";
   } else if (
     (playerSelection === 'rock' && computerSelection === 'scissors')
     || (playerSelection === 'scissors' && computerSelection === 'paper')
     || (playerSelection === 'paper' && computerSelection === 'rock')
   ) {
+    win += 1;
     return `You win! ${playerSelection} beats ${computerSelection}`;
   } else {
+    loss += 1;
     return `You lose! ${computerSelection} beats ${playerSelection}`;
   }
 }
+
+
+let win = 0;
+let loss = 0;
+let tie = 0;
