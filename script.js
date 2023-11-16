23fc6ce0c893c0239b9da43ca36840f9cdfd61cc
xytos99 <134260005+xytos99@users.noreply.github.com>
2023-11-16 11:42:15 +0530

    Add function playRound()
    This function facilitates a single round of the game and declares the
    result

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function getComputerChoice() {
   let randomChoice = choices[Math.floor(Math.random() * choices.length)];
   return randomChoice;
 }
+
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
+  if ( playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors')
+    || (playerSelection === 'scissors' && computerSelection === 'paper')
+    || (playerSelection === 'paper' && computerSelection === 'rock')
+  ) {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
