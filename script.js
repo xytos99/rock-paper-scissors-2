ed3681964f108da461e2c33a05f4bfc28a21e92a
xytos99 <134260005+xytos99@users.noreply.github.com>
2023-12-15 12:28:33 +0530

    Add a div for displaying results
    Change 'console.log' into DOM methods

--- script.js
+++ script.js
@@ -30,38 +30,27 @@ let loss = 0;
 let tie = 0;
 
 let body = document.querySelector('body');
+let result = document.querySelector('#result');
 
 body.addEventListener('click', (e) => {
   let target = e.target;
 
   switch(target.id) {
     case 'rock':
-      console.log(playRound('rock', getComputerChoice()));
+      result.textContent = playRound('rock', getComputerChoice());
       break;
     case 'paper':
-      console.log(playRound('paper', getComputerChoice()));
+      result.textContent = playRound('paper', getComputerChoice());
       break;
     case 'scissors':
-      console.log(playRound('scissors', getComputerChoice()));
+      result.textContent = playRound('scissors', getComputerChoice());
       break;
   }
 })
 
 function game() {
   
-  console.log(`Win: ${win} Loss: ${loss} Tie: ${tie}`);
 
-  let result;
-  if (win === loss) {
-    result = "It's a Tie!";
-  } else if (win > loss) {
-    result = "You Win!";
-  } else {
-    result = "You Lose!";
-  }
-
-  console.log(""); // Adds a blank line before the result
-  console.log(result);
 }
 
 game();
